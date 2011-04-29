bd45d82ea4db6cb220d50b0beddc1f7712a1654c
HaroldGiraldo <harold@hlmbusiness.com>
2011-04-29 13:09:27 -0500

    Add sing up button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'dashboard'
 
+  map.singup '/singup', :controller => 'users', :action => 'new'
   map.login '/login', :controller => 'user_sessions', :action => 'new'
   map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
 
